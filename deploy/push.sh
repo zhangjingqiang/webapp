61c49a6a4e6d3ea9013a4adf5aa80b4860929064
zhangjingqiang <zhangjingqiang@gmail.com>
2017-05-03 21:36:18 +0900

    Automation Scripts

--- deploy/push.sh
+++ deploy/push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -x
 
 LC=$(git rev-parse --short HEAD)
 docker build -t zhangjingqiang/webapp:${LC} .
 docker push zhangjingqiang/webapp:${LC}
+kubectl set image deployment webapp webapp=zhangjingqiang/webapp:${LC}
